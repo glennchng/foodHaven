44e4c3952dcac20170e0a97b94bbded58dcad6cc
glennchng <zhixiang.chng@my.jcu.edu.au>
2018-09-11 11:05:08 +0800

    qr

--- app/src/main/java/com/example/glen9/foodhaven/qrcode.java
+++ app/src/main/java/com/example/glen9/foodhaven/qrcode.java
@@ -1,6 +1,7 @@
 package com.example.glen9.foodhaven;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -12,130 +13,55 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.zxing.Result;
-
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import static android.Manifest.permission_group.CAMERA;
 
-public class qrcode extends AppCompatActivity implements ZXingScannerView.ResultHandler{
-
-    private static final int requestCamera = 1;
-    private ZXingScannerView scannerView;
+public class qrcode extends AppCompatActivity {
+    private Button scanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        scannerView = new ZXingScannerView(this);
-        setContentView(scannerView);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            if(checkPermission()){
-                Toast.makeText(qrcode.this,"Permission granted!",Toast.LENGTH_LONG).show();
-            }else
-            {
-                requestPermission();
-            }
-        }
-    }
-
-    private boolean checkPermission(){
-        return (ContextCompat.checkSelfPermission(qrcode.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED);
-    }
-
-    private void requestPermission(){
-        ActivityCompat.requestPermissions(this,new String[]{CAMERA},requestCamera);
-    }
-
-    public void onRequestPermissionResult(int requestCode, String permission[], int grantResults[]){
-        switch(requestCode){
-            case requestCamera:
-                if(grantResults.length > 0){
-                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    if(cameraAccepted){
-                        Toast.makeText(qrcode.this, "Permission granted!",Toast.LENGTH_LONG).show();
-                    }
-                    else
-                    {
-                        Toast.makeText(qrcode.this, "Permission denied!",Toast.LENGTH_LONG).show();
-                        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                            if(shouldShowRequestPermissionRationale(CAMERA)){
-                                displayAlertMessage("You need to allow access for both permissions",
-                                        new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialogInterface, int i) {
-                                                requestPermissions(new String[]{CAMERA},requestCamera);
-                                            }
-                                        });
-                                return;
-                            }
-                        }
-                    }
-                }
-                break;
-        }
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-        {
-            if(checkPermission())
-            {
-                if(scannerView == null)
-                {
-                    scannerView = new ZXingScannerView(this);
-                    setContentView(scannerView);
-                }
-                scannerView.setResultHandler(this);
-                scannerView.startCamera();
-            }
-            else{
-                requestPermission();
-            }
-        }
-    }
-
-    @Override
-    public void onDestroy()
-    {
-        super.onDestroy();
-        scannerView.stopCamera();
-    }
-
-    public void displayAlertMessage(String message, DialogInterface.OnClickListener listener)
-    {
-        new AlertDialog.Builder(qrcode.this)
-                .setMessage(message)
-                .setPositiveButton("OK",listener)
-                .setNegativeButton("CANCEL",listener)
-                .create()
-                .show();
-    }
-
-    @Override
-    public void handleResult(final Result result) {
-        final String scanResult = result.getText();
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Scan Result");
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        setContentView(R.layout.activity_qrcode);
+        scanButton = (Button) findViewById(R.id.scanButton);
+        final Activity activity = this;
+        scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                scannerView.resumeCameraPreview(qrcode.this);
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
             }
         });
-        builder.setNeutralButton("Visit", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(scanResult));
-                startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Scanning cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
             }
-        });
-        builder.setMessage(scanResult);
-        AlertDialog alert = builder.create();
-        alert.show();
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
+
+
+
+
